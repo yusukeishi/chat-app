742452f4a8c6d2f211e18308fce3685732dffa01
yusukeishi <ishisida777@icloud.com>
2021-09-06 18:57:56 +0900

    メッセージ送信機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-    has_many :room_user
+    has_many :room_users
     has_many :rooms, through: :room_users
     has_many :messages
        
